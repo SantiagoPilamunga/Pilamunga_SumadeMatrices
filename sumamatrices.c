d12f688bbe27ab43f40f45a9b6cef48898e274f9
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-28 15:00:54 -0500

    imprimimos las dos matrices y la matriz resultante de la suma de las dos matrices

--- sumamatrices.c
+++ sumamatrices.c
@@ -10,32 +10,37 @@ int main()
     scanf("%d", &m);
     srand(time(NULL));
     int matrix[n][m];
+    printf("Primera matriz \n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             matrix[i][j]=rand()%101;
+            printf("%d\t", matrix[i][j]);
         }
-        
+        printf("\n");
     }
     int matrix2[n][m];
+    printf("Segunda matriz \n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             matrix2[i][j]=rand()%101;
+            printf("%d\t", matrix2[i][j]);
         }
-        
+        printf("\n");
     }
     int suma[n][m];
+    printf("Suma de primera y segunda matriz \n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             suma[i][j]=matrix[i][j]+matrix2[i][j];
+            printf("%d\t", suma[i][j]);
         }
-        
+        printf("\n");
     }
-    
     return 0;
 }
