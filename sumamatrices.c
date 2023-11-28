c9c67054f5d115d9a7043b80539dce44b8a743b0
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-28 14:53:44 -0500

    matriz suma

--- sumamatrices.c
+++ sumamatrices.c
@@ -27,6 +27,15 @@ int main()
         }
         
     }
+    int suma[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            suma[i][j]=matrix[i][j]+matrix2[i][j];
+        }
+        
+    }
     
     return 0;
 }
