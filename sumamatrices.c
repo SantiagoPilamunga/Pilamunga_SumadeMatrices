17af275ac35842dedfa2fed7992f6facff755aa7
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-28 14:47:18 -0500

    DECLARACION DE MATRIZ Y ASIGNACION DE VALORES

--- sumamatrices.c
+++ sumamatrices.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
     int n,m;
     printf("Ingrese el numero de filas: ");
     scanf("%d", &n);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &m);
+    srand(time(NULL));
+    int matrix[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            matrix[i][j]=rand()%101;
+        }
+        
+    }
+    
     return 0;
 }
